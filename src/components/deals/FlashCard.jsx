80733358095ff2f12557dcf1de3b5cda78111189
hafsa-nuh <109968509+hafsa-nuh@users.noreply.github.com>
2022-11-10 13:25:47 +0300

    deal cards fixed

--- src/components/deals/FlashCard.jsx
+++ src/components/deals/FlashCard.jsx
@@ -1,47 +1,50 @@
-import React, { useEffect, useState } from "react"
-import Slider from "react-slick"
+import React, { useEffect, useState } from "react";
+import Slider from "react-slick";
 import {
   AiTwotoneStar,
   AiOutlineStar,
   AiOutlinePlus,
-  AiOutlineHeart,
+  AiFillHeart,
 } from "react-icons/ai";
-import "slick-carousel/slick/slick.css"
-import "slick-carousel/slick/slick-theme.css"
+import { FaLongArrowAltLeft, FaLongArrowAltRight } from "react-icons/fa";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const SampleNextArrow = (props) => {
-  const { onClick } = props
+  const { onClick } = props;
   return (
-    <div className='control-btn' onClick={onClick}>
-      <button className='next'>
-        <i className='fa fa-long-arrow-alt-right'></i>
+    <div className="control-btn" onClick={onClick}>
+      <button className="next">
+        <FaLongArrowAltRight className="next-btn" />
+        {/* <i className='fa fa-long-arrow-alt-right'></i> */}
       </button>
     </div>
-  )
-}
+  );
+};
 const SamplePrevArrow = (props) => {
-  const { onClick } = props
+  const { onClick } = props;
   return (
-    <div className='control-btn' onClick={onClick}>
-      <button className='prev'>
-        <i className='fa fa-long-arrow-alt-left'></i>
+    <div className="control-btn" onClick={onClick}>
+      <button className="prev">
+        <FaLongArrowAltLeft className="next-btn" />
+        {/* <i className='fa fa-long-arrow-alt-left'></i> */}
       </button>
     </div>
-  )
-}
+  );
+};
 const FlashCard = () => {
   const [productItems, setProductItems] = useState([]);
-  console.log(productItems)
+  console.log(productItems);
   useEffect(() => {
     fetch("http://localhost:9292/products")
       .then((r) => r.json())
       .then((data) => setProductItems(data));
   }, []);
 
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   const increment = () => {
-    setCount(count + 1)
-  }
+    setCount(count + 1);
+  };
   const settings = {
     dots: false,
     infinite: true,
@@ -50,31 +53,36 @@ const FlashCard = () => {
     slidesToScroll: 1,
     nextArrow: <SampleNextArrow />,
     prevArrow: <SamplePrevArrow />,
-  }
+  };
 
   return (
     <>
       <Slider {...settings}>
-        {productItems.map((items , id) => {
+        {productItems.map((items, id) => {
           return (
             <div className="box">
               <div className="product mtop" key={id}>
                 <div className="img">
-                  <span className="discount">{items.name}</span>
+                  {/* <span className="discount">{items.name}</span> */}
                   <img src={items.image_url} alt={items.name} />
-                  <div className="product-like">
-                    <label>{count}</label> <br />
-                    <AiOutlineHeart onClick={increment} />
-                  </div>
                 </div>
                 <div className="product-details">
-                  <h3>{items.description}</h3>
+                  <div className="product-like">
+                    <label>{count}</label> <br />
+                    <AiFillHeart className="heart" onClick={increment} />
+                  </div>
+                  <br />
+                  <h3>{items.name}</h3>
+                  {/* <br /> */}
+                  <div className="paragra">
+                    <p>{items.description}</p>
+                  </div>
                   <div className="rate">
-                    <AiTwotoneStar />
-                    <AiTwotoneStar />
-                    <AiTwotoneStar />
-                    <AiTwotoneStar />
-                    <AiOutlineStar />
+                    <AiTwotoneStar className="icons" />
+                    <AiTwotoneStar className="icons" />
+                    <AiTwotoneStar className="icons" />
+                    <AiTwotoneStar className="icons" />
+                    <AiOutlineStar className="icon" />
                   </div>
                   <div className="price">
                     <h4>Ksh {items.price}.00 </h4>
@@ -89,7 +97,7 @@ const FlashCard = () => {
         })}
       </Slider>
     </>
-  )
-}
+  );
+};
 
-export default FlashCard
+export default FlashCard;
